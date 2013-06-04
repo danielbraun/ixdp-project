e00cb13d4b3bf6cac60dd4a7f0c4d5e99aa073b8
Daniel Braun <dbraun86@gmail.com>
2013-06-04 20:37:19 +0000

    added basic d3 template

--- visualization/urls.py
+++ visualization/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
+from django.views.generic.base import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'visualization.views.home', name='home'),
-    # url(r'^visualization/', include('visualization.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns = patterns(
+    '',
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
